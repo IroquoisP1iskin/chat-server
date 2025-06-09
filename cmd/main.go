c52ef19fe0b454fed36fc00864533de80d680d2d
semiannikov <semyannikovnd@softapro.pro>
2025-06-09 23:27:20 +0700

    2025/06/09 23:24:43 Note: id:23  name:"Solid Snake"  email:"bitch@suckmycock.dildo"  role:ADMIN  created_at:{seconds:1749486283  nanos:253818843}  updated_at:{seconds:1749486283  nanos:253819703} 2025/06/09 23:24:43 Note: bitch@suckmycock.dildo

--- cmd/main.go
+++ cmd/main.go
@@ -4,14 +4,20 @@ import (
 	"context"
 	"fmt"
 	"github.com/IroquoisP1iskin/chat-server/pkg/chat_v1"
+	"github.com/fatih/color"
 	"google.golang.org/grpc"
-	"google.golang.org/grpc/reflection"
+	"google.golang.org/grpc/credentials/insecure"
 	"google.golang.org/protobuf/types/known/emptypb"
 	"log"
-	"net"
+	"time"
+
+	desc "github.com/IroquoisP1iskin/auth/pkg/note_v1"
 )
 
-const address = "127.0.0.1:50001"
+const (
+	address = "localhost:50051"
+	noteID  = 23
+)
 
 type server struct {
 	chat_v1.UnimplementedChatServiceServer
@@ -33,17 +39,23 @@ func (s *server) Send(_ context.Context, req *chat_v1.SendMessageRequest) (*empt
 }
 
 func main() {
-	lis, err := net.Listen("tcp", address)
+	conn, err := grpc.Dial(address, grpc.WithTransportCredentials(insecure.NewCredentials()))
 	if err != nil {
-		log.Fatalf("Failed to create listener: %s", err.Error())
+		log.Fatalf("did not connect: %v", err)
+	}
+	defer conn.Close()
+
+	c := desc.NewAuthClient(conn)
+
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+	defer cancel()
+
+	r, err := c.Get(ctx, &desc.GetRequest{Id: noteID})
+	if err != nil {
+		log.Fatalf("could not get note: %v", err)
+	} else {
+		log.Printf("Note: %v", r)
 	}
 
-	grpcServer := grpc.NewServer()
-	reflection.Register(grpcServer)
-	chat_v1.RegisterChatServiceServer(grpcServer, &server{})
-
-	err = grpcServer.Serve(lis)
-	if err != nil {
-		log.Fatalf("Failed to serve: %s", err.Error())
-	}
+	log.Printf("%s%s", color.GreenString("Note:"), color.GreenString(" %v", r.GetEmail()))
 }
--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/IroquoisP1iskin/chat-server
 go 1.23.0
 
 require (
+	github.com/IroquoisP1iskin/auth v0.0.0-20250609153014-da2bbb463480
 	github.com/fatih/color v1.18.0
 	google.golang.org/grpc v1.73.0
 	google.golang.org/protobuf v1.36.6
@@ -15,5 +16,4 @@ require (
 	golang.org/x/sys v0.33.0 // indirect
 	golang.org/x/text v0.26.0 // indirect
 	google.golang.org/genproto/googleapis/rpc v0.0.0-20250603155806-513f23925822 // indirect
-	google.golang.org/grpc/cmd/protoc-gen-go-grpc v1.5.1 // indirect
 )
